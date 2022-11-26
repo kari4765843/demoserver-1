c03f4a64743f23bab2ec497aee0f57214902ded6
karina A <karina.arevalo@hotmail.com>
2022-11-26 17:49:33 -0600

    Doing some changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,26 @@
 import { Router } from 'express'
+import basicAuth from 'express-basic-auth'
 
-import root from './root'
-import api from './api'
-
+import persons from './persons'
+import companies from './companies'
+import departments from './departments'
+import employees from './employees'
 
 const router = Router()
 
-router.use('/', root)
-router.use('/api', api)
+router.use(
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  }),
+)
+
+router.get('/', (req, res) => {
+  res.send({ msg: 'Inside API Endpoints' })
+})
+
+router.use('/persons', persons)
+router.use('/companies', companies)
+router.use('/departments', departments)
+router.use('/employees', employees)
 
 export default router
